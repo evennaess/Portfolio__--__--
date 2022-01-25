45ac3c2ba343553f9a56f7ad49feb0ecc8772bf2
evennaess <74900310+evennaess@users.noreply.github.com>
2022-01-25 21:29:53 +0100

    Initial commit

---
+++ www/script.js
@@ -0,0 +1,12 @@
+function init() {
+    const burgerIcon = document.querySelector('.burger-icon');
+    const mobileNav = document.querySelector('.mobile-nav');
+    burgerIcon.addEventListener('click', () => {
+        mobileNav.classList.toggle('mobile-nav-hide');
+        burgerIcon.classList.toggle('burger');
+        burgerIcon.classList.toggle('closemobilemenu');
+    }); 
+    
+}
+
+init();
