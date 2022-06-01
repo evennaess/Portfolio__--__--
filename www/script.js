ae8ae736bcd490e4f7c6039ce50ab3e0cc8d2453
evennaess <74900310+evennaess@users.noreply.github.com>
2022-06-01 15:39:39 +0200

    Portfolio 2 commit

--- www/script.js
+++ www/script.js
@@ -15,15 +15,17 @@ function init() {
 
 async function getSanity() {
     const clientId = '7cjl1phd';
-    const url = `https://${clientId}.api.sanity.io/v2021-10-21/data/query/production?query=*[]`;
+    const url = `https://${clientId}.api.sanity.io/v2021-10-21/data/query/production?query=*[_type=='project']`;
     const response = await fetch(url);
     const results = await response.json()
-    console.log(results)
+    console.log(results.result)
+
+        
 }
 
 
 init();
-getSanity();
+//getSanity();
 const windowWidth = $(document).width()
 
     console.log('kjør')
@@ -43,3 +45,32 @@ const windowWidth = $(document).width()
     $('a').on('mouseleave', function(){
         $('.cursor').removeClass('cursorExpand')
     })
+
+
+var transitionSpeed = 1;
+function animateContent() {          
+    TweenMax.set('.borderAdjustment', {width: 0});
+    TweenMax.set('.borderAdjustment-bottom', {width: 0});
+    TweenMax.set('.bodytextParagraphOne, .bodytextParagraphTwo, .footerContainer, #digital, #designer, .outlined, #developer', {opacity: 0});
+    TweenMax.set('.main-menu, #digital, #designer, .outlined, #developer ', {opacity: 0, y:25 });
+    TweenMax.set('.footerContainer ', {opacity: 0, y:-25 });
+    TweenMax.set('.day-nightElem', {opacity: 0, scale:0 });
+
+
+
+    TweenMax.to('.borderAdjustment', transitionSpeed, {width: '100%', delay: .5})
+    TweenMax.to('.borderAdjustment-bottom', transitionSpeed, {width: '85%', delay: .5})
+    TweenMax.to('.main-menu', transitionSpeed / 2, {opacity: 1, y: 0, delay: 2})
+    TweenMax.to('.projectGrid', transitionSpeed / 3, {opacity: 1, delay: 2.5})
+    TweenMax.to('.day-nightElem', transitionSpeed / 2, {opacity: 1, scale: 1, delay: 1.5})
+    
+    TweenMax.to('.day-nightElem', transitionSpeed / 2, {opacity: 1, scale: 1, delay: 1.5})
+
+    TweenMax.to('.footerContainer', transitionSpeed, {opacity: 1, y: 0, delay: 2})
+    TweenMax.to('#digital', transitionSpeed / 2, {opacity: 1, delay: .6})
+    TweenMax.to('#designer', transitionSpeed / 2, {opacity: 1, delay: .8})
+    TweenMax.to('.outlined', transitionSpeed / 2, {opacity: 1, delay: 1})
+    TweenMax.to('#developer', transitionSpeed / 2, {opacity: 1, delay: 1.2})
+    // TweenMax.to('.bodytextParagraphOne, .bodytextParagraphTwo', transitionSpeed, {opacity: 1, delay: 2.5})
+}
+animateContent();
